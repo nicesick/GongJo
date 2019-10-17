ade573ca4a32371153aecc96365c469a5a274525
jkim0715 <gjr191@naver.com>
2019-10-17 16:51:14 +0900

    Edit ECU simulator

--- ECUSimulator/app/src/main/java/com/example/ecusimulator/MainActivity.java
+++ ECUSimulator/app/src/main/java/com/example/ecusimulator/MainActivity.java
@@ -15,8 +15,8 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textview, textview2, textview3,textViewSpeed,textViewDistance,textViewFuel,textViewBattery,textViewCO2,textViewDust,textViewSdust,textViewTemperature,textViewHumidity;
-    SeekBar seekBar, seekBar2, seekBar3,seekBar4,seekBar5, seekBar6, seekBar7,seekBar8,seekBar9;
+    TextView textview, textview2, textview3,textViewSpeed,textViewDistance,textViewFuel,textViewBattery,textViewCO2,textViewDust,textViewSdust,textViewTemperature,textViewHumidity,textViewAircon,textViewEngine,textViewBrake,textViewAccoil,textViewCoolwater,textViewAccPressure,textViewBrakePressure;
+    SeekBar seekBar, seekBar2, seekBar3,seekBar4,seekBar5, seekBar6, seekBar7,seekBar8,seekBar9,seekBar10,seekBar11,seekBar12,seekBar13,seekBar14,seekBar15,seekBar16;
     Button button2,button3;
 
 
@@ -42,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
         seekBar7 = findViewById(R.id.seekBar7);
         seekBar8 = findViewById(R.id.seekBar8);
         seekBar9 = findViewById(R.id.seekBar9);
+        seekBar10 = findViewById(R.id.seekBar10);
+        seekBar11 = findViewById(R.id.seekBar11);
+        seekBar12 = findViewById(R.id.seekBar12);
+        seekBar13 = findViewById(R.id.seekBar13);
+        seekBar14 = findViewById(R.id.seekBar14);
+        seekBar15 = findViewById(R.id.seekBar15);
+        seekBar16 = findViewById(R.id.seekBar16);
 
         seekBar.setMax(200);
         seekBar2.setMax(1000);
@@ -62,6 +69,13 @@ public class MainActivity extends AppCompatActivity {
         textViewDistance = findViewById(R.id.textViewDistance);
         textViewFuel = findViewById(R.id.textViewFuel);
         textViewBattery = findViewById(R.id.textViewBattery);
+        textViewAircon = findViewById(R.id.textViewAircon);
+        textViewEngine = findViewById(R.id.textViewEngine);
+        textViewBrake = findViewById(R.id.textViewBrake);
+        textViewAccoil = findViewById(R.id.textViewAccoil);
+        textViewCoolwater = findViewById(R.id.textViewCoolwater);
+        textViewAccPressure = findViewById(R.id.textViewAccPressure);
+        textViewBrakePressure = findViewById(R.id.textViewBrakePressure);
 
 
 
@@ -472,6 +486,314 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        seekBar10.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int  aircon = seekBar10.getProgress();
+                textViewAircon.setText(aircon + "");
+                String temp = "";
+                if(aircon<10){
+                    temp = "00"+aircon;
+                }else if(aircon<100){
+                    temp = "0"+aircon;
+                }else{
+                    temp = ""+aircon;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300600000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
+        seekBar11.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int engine = seekBar11.getProgress();
+                textViewEngine.setText(engine + "");
+                String temp = "";
+                if(engine<10){
+                    temp = "00"+engine;
+                }else if(engine<100){
+                    temp = "0"+engine;
+                }else{
+                    temp = ""+engine;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300700000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
+        seekBar12.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int  brakeoil = seekBar12.getProgress();
+                textViewBrake.setText(brakeoil + "");
+                String temp = "";
+                if(brakeoil<10){
+                    temp = "00"+brakeoil;
+                }else if(brakeoil<100){
+                    temp = "0"+brakeoil;
+                }else{
+                    temp = ""+brakeoil;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300750000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
+        seekBar13.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int accoil = seekBar13.getProgress();
+                textViewAccoil.setText(accoil + "");
+                String temp = "";
+                if(accoil<10){
+                    temp = "00"+accoil;
+                }else if(accoil<100){
+                    temp = "0"+accoil;
+                }else{
+                    temp = ""+accoil;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300800000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
+        seekBar14.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int  cool = seekBar14.getProgress();
+                textViewCoolwater.setText(cool + "");
+                String temp = "";
+                if(cool<10){
+                    temp = "00"+cool;
+                }else if(cool<100){
+                    temp = "0"+cool;
+                }else{
+                    temp = ""+cool;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300850000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
+        seekBar15.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int  accpad = seekBar15.getProgress();
+                textViewAccPressure.setText(accpad + "");
+                String temp = "";
+                if(accpad<10){
+                    temp = "00"+accpad;
+                }else if(accpad<100){
+                    temp = "0"+accpad;
+                }else{
+                    temp = ""+accpad;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300900000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
+        seekBar16.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                final int brakepad = seekBar16.getProgress();
+                textViewBrakePressure.setText(brakepad + "");
+                String temp = "";
+                if(brakepad<10){
+                    temp = "00"+brakepad;
+                }else if(brakepad<100){
+                    temp = "0"+brakepad;
+                }else{
+                    temp = ""+brakepad;
+                }
+                final String finalTemp = temp;
+                Runnable testSendIoTRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            out = socket.getOutputStream();
+                            dout = new DataOutputStream(out);
+                            dout.writeUTF("000300950000000000000"+ finalTemp);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                Thread testThread = new Thread(testSendIoTRunnable);
+                testThread.start();
+            }
+        });
     }//End OnCreate
 
 
