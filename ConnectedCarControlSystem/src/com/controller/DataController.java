56bf1620cc99e958fc7d6b38e58d80c30ceade5b
Aha <mistel1327@gmail.com>
2019-10-22 17:32:00 +0900

    Modify Log4j For Test & Add TestFiles for test with Hive

--- ConnectedCarControlSystem/src/com/controller/DataController.java
+++ ConnectedCarControlSystem/src/com/controller/DataController.java
@@ -1,17 +1,22 @@
 package com.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.test.PrintLog;
-import com.vo.CarStatus;
+import com.vo.CarStatusTestHive;
 
 @Controller
 public class DataController {
@@ -34,5 +39,38 @@ public class DataController {
 	public void getData(String car_id) {
 		PrintLog.printLog("DataController", "Request Accepted");
 		PrintLog.printLog("DataController", car_id);
+	}
+	
+	@RequestMapping("getDataFromHive.mc")
+	public ModelAndView getDataFromHive(ModelAndView mv) {
+		ArrayList<CarStatusTestHive> carStatus = new ArrayList<CarStatusTestHive>();
+		
+		try {
+			Class.forName("org.apache.hive.jdbc.HiveDriver");
+			Connection conn = DriverManager.getConnection("jdbc:hive2://70.12.60.103:10000/default", "", "");
+	        Statement stmt = conn.createStatement();
+	        ResultSet rs = stmt.executeQuery("SELECT * FROM test_hive");
+	        
+	        while (rs.next()) {
+	            System.out.println(rs.getDate("car_date"));
+	            System.out.println(rs.getString("car_id"));
+	            
+	            carStatus.add(new CarStatusTestHive(rs.getDate("car_date"), rs.getString("car_id")));
+	        }
+
+	        conn.close();
+	        System.out.println("Success....");
+	        
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		mv.setViewName("index");
+		mv.addObject("center", "testHive");
+		mv.addObject("hiveInfo", carStatus);
+		
+		return mv;
 	}
 }
