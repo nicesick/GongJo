a1e5ccf6fb0eae10e10cc1717a787014c58139a5
Aha <mistel1327@gmail.com>
2019-10-25 12:37:48 +0900

    Delete com Folder

--- com/callback/SocketDataCallback.java
+++
@@ -1,8 +0,0 @@
-package com.callback;
-
-import com.socket.Receiver;
-
-public interface SocketDataCallback {
-	public void registerSocket(Receiver receiver, String car_id);
-	public void removeSocket(Receiver receiver, String car_id);
-}
--- com/test/PrintLog.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public class PrintLog {
-	public static void printLog(String tag, String msg) {
-		System.out.println(tag + " : " + msg);
-	}
-}
