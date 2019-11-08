6c31731aad5b381832842d68c89951f4421efa41
JYn <1616jojo@naver.com>
2019-11-08 16:55:16 +0900

    modify socket retry

--- can/src/can/Main.java
+++ can/src/can/Main.java
@@ -3,8 +3,8 @@ package can;
 public class Main {
 	public static void main(String[] args) {
 		try {
-//			SerialTestS st = new SerialTestS("COM7","70.12.227.247", 8888);	
-			SerialTestS st = new SerialTestS("COM7","70.12.225.203", 8888);
+			SerialTestS st = new SerialTestS("COM8","70.12.227.247", 8800);
+//			SerialTestS st = new SerialTestS("COM7","70.12.225.203", 8888);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
